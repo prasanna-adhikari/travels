2b9cdb8eb3c0709b2e2a541ac104a031544847a6
prasanna-adhikari <prasannaadhikari022@gmail.com>
2022-05-28 21:12:18 +0545

    polpular destination

--- components/Card/Destinations.js
+++ components/Card/Destinations.js
@@ -6,6 +6,8 @@ import {
   Text,
   Stack,
   Avatar,
+  VStack,
+  Flex,
   // Image,
   useColorModeValue,
 } from "@chakra-ui/react";
@@ -22,61 +24,39 @@ export default function Destinations() {
         // p={6}
         overflow={"hidden"}
       >
-        <Stack>
-          <Box
-            h={"280px"}
-            bg={"gray.100"}
-            mt={-6}
-            mx={-6}
-            // mb={-6}
-            pos={"relative"}
+        <Flex
+          w={"full"}
+          h={"250px"}
+          backgroundImage={
+            "url(https://images.unsplash.com/photo-1611516491426-03025e6043c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1333&q=80)"
+          }
+          backgroundSize={"cover"}
+          backgroundPosition={"center center"}
+        >
+          <Stack
+            w={"full"}
+            justify={"end"}
+            // px={useBreakpointValue({ base: 4, md: 8 })}
+            bgGradient={
+              "linear(to-tr, blackAlpha.600, transparent,transparent)"
+            }
           >
-            <Image
-              src={
-                "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
-              }
-              layout={"fill"}
-            />
-          </Box>
-          <Text>Nepal</Text>
-        </Stack>
-        {/* <Stack>
-          <Text
-            color={"green.500"}
-            textTransform={"uppercase"}
-            fontWeight={800}
-            fontSize={"sm"}
-            letterSpacing={1.1}
-          >
-            Blog
-          </Text>
-          <Heading
-            color={useColorModeValue("gray.700", "white")}
-            fontSize={"2xl"}
-            fontFamily={"body"}
-          >
-            Boost your conversion rate
-          </Heading>
-          <Text color={"gray.500"} noOfLines={[4, 6]}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Lorem ipsum dolor sit amet, consetetur sadipscing
-            elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-            magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-            justo duo dolores et ea rebum.
-          </Text>
-        </Stack> */}
-        {/* <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
-          <Avatar
-            src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
-            alt={"Author"}
-          />
-          <Stack direction={"column"} spacing={0} fontSize={"sm"}>
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={"gray.500"}>Feb 08, 2021 · 6min read</Text>
+            <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
+              <Text
+                color={"white"}
+                fontWeight={600}
+                lineHeight={1.2}
+                fontSize="lg"
+                letterSpacing={1}
+                pl={4}
+                pb={3}
+                // fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
+              >
+                Nepal
+              </Text>
+            </Stack>
           </Stack>
-        </Stack> */}
+        </Flex>
       </Box>
     </Center>
   );
